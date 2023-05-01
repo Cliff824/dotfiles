1620dd29c50abd2516d2ea01c8812acf63b37089
Cliff824 <not@aemail>
2023-05-01 13:01:46 +0100

    update

--- nvim/lua/core/keybinds.lua
+++ nvim/lua/core/keybinds.lua
@@ -8,6 +8,7 @@ keymap.set("n", "<leader>sv", "<C-w>v") --vertical split
 keymap.set("n", "<leader>sh", "<C-w>s") --horizontal split
 keymap.set("n", "<leader>sx", ":close<CR>") --close current split
 
+keymap.set("n", "<leader>t", ":TransparentToggle<CR>")
 --plugin keymaps
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 
